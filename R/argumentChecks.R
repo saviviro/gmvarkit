97567d7f9f642c5a1f6d93bdfc454023247b5e39
saviviro <43138478+saviviro@users.noreply.github.com>
2023-04-03 11:11:52 +0300

    updated documentation

--- R/argumentChecks.R
+++ R/argumentChecks.R
@@ -316,7 +316,6 @@ check_parameters <- function(p, M, d, params, model=c("GMVAR", "StMVAR", "G-StMV
 #' @inheritParams is_stationary
 #' @return Checks the constraint matrix \strong{C} and throws an error
 #'   if something is wrong.
-#' @details If \code{is.null(constraints)}, then this function doesn't do anything.
 #' @keywords internal
 
 check_constraints <- function(p, M, d, constraints=NULL, same_means=NULL, structural_pars=NULL) {
